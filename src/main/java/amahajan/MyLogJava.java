d502fbd560c04edda2079737844e236db7d98615
Ashish Mahajan <amahajan@chenoainc.com>
2018-01-24 17:08:10 +0530

    First Commit

---
+++ src/main/java/amahajan/MyLogJava.java
@@ -0,0 +1,28 @@
+package amahajan;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MyLogJava {
+	
+	public static void main(String args[]) throws IOException {
+		System.out.println("Hello!!!");
+		BufferedWriter out = null;
+		try {
+		    out = new BufferedWriter(new FileWriter("/shared/test.txt"));
+		    out.write("aString\nthis is a\nttest");  //Replace with the string 
+		                                             //you are trying to write
+		}
+		catch (IOException e)
+		{
+		    System.out.println("Exception ");
+
+		}
+		finally
+		{
+		    out.close();
+		}
+	}
+
+}
